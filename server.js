f43d08ce11315a0641204c3d807b88464f205e91
Shaun Domingo <sdomingo@macquarietelecom.com>
2018-04-19 22:31:40 +1000

    Minor copy change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var os = require('os');
 var handleRequest = function(request, response) {
   console.log('Received request for URL: ' + request.url);
   response.writeHead(200);
-  response.end('Hello World!\nContainer ' + os.hostname());
+  response.end('Hello Earth!\nContainer ' + os.hostname());
 };
 var www = http.createServer(handleRequest);
 www.listen(8080);
